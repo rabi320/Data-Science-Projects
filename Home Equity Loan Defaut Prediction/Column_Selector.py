bbf3fabe90f761a8c77780c0d9300d48b112e461
rabi320 <ryonatan6@gmail.com>
2021-07-16 17:56:26 +0300

    Change
    18:00

---
+++ Home Equity Loan Defaut Prediction/Column_Selector.py
@@ -0,0 +1,14 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class Selector(BaseEstimator, TransformerMixin):
+    """
+    Transformer to select a single column from the data frame to perform additional transformations on
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.key]
